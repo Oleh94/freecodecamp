3d90481d81881ef01a536ff3ae7fae25d5a0734a
Oleh Vasylenko <61271382+Oleh94@users.noreply.github.com>
2021-10-28 00:49:19 +0300

    Add files via upload

---
+++ basic_js75.js
@@ -0,0 +1,23 @@
+function caseInSwitch(val) {
+    let answer = "";
+    // Only change code below this line
+    switch (val){
+        case 1:
+            val = "alpha";
+            break;
+        case 2:
+            val = "beta";
+            break;
+        case 3:
+            val = "gamma";
+            break;
+        case 4:
+            val = "delta";
+            break
+    }
+    answer = val;
+    // Only change code above this line
+    return answer;
+}
+
+caseInSwitch(1);
